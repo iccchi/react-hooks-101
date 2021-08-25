2e31e9fb4645cc39688c9f2f3628bd26dd3e86ec
iccchi <tmy_1217@yahoo.co.jp>
2021-08-26 00:50:03 +0900

    solve prop drilling issue

--- src/components/Events.js
+++ src/components/Events.js
@@ -3,8 +3,8 @@ import React,{useContext} from 'react';
 import {Event} from './Event'
 import { AppContext } from '../contexts/AppContext';
 
-export const Events = ({state, dispatch}) => {
-  const value = useContext(AppContext)
+export const Events = () => {
+  const {state} = useContext(AppContext)
   return(
     <React.Fragment>
       <h4>イベント一覧</h4>
@@ -18,7 +18,7 @@ export const Events = ({state, dispatch}) => {
             </tr>
           </thead>
           <tbody>
-            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch} />))}
+            {state.map((event, index) => (<Event key={index} event={event}  />))}
           </tbody>
 
         </table>
